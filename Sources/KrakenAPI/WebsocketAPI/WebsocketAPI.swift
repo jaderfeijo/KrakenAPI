43a84bc48f32994676c0668e63cd6bbe5302ea2f
Jader Feijo <jaderfeijo@jaders-ubuntu-mbp.home>
2022-02-02 10:19:49 +0000

    Implement websocket hostname method + tests

--- Sources/KrakenAPI/WebsocketAPI/WebsocketAPI.swift
+++ Sources/KrakenAPI/WebsocketAPI/WebsocketAPI.swift
@@ -9,3 +9,20 @@ public extension Kraken {
 		}
 	}
 }
+
+// MARK: - Internal -
+
+extension Kraken.Configuration.Access {
+	func websocketHostname(for environment: Kraken.Configuration.Environment) -> String {
+		switch (environment, self) {
+		case (.production, .public):
+			return "ws.kraken.com"
+		case (.production, .authenticated):
+			return "ws-auth.kraken.com"
+		case (.beta, .public):
+			return "beta-ws.kraken.com"
+		case (.beta, .authenticated):
+			return "beta-ws-auth.kraken.com"
+		}
+	}
+}
